a121ce06a8095d27fa96630db11a0401dee72745
PedroHenriqueCos <phlkpedro@gmail.com>
2024-11-08 11:52:34 -0300

    primeiro commit

---
+++ estudo.php
@@ -0,0 +1,19 @@
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>PHP</title>
+    </head>
+    <body>
+        <?php
+         echo "Este trecho de código foi inserido antes da tag HTML <h1>"
+        ?>
+        
+        <h1>Introdução ao PHP</h1>
+
+        <?php
+        echo "Este trecho de código foi inserido antes da tag HTML <h1>"
+       ?>
+
+    </body>
+</html>    
